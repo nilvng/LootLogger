dca6f6e6508289644c728754cad5e48925ca9db3
Nil Nguyen <Nilver@air-cua-mac.stusgs.rmit.edu.vn>
2021-03-01 08:42:57 +0700

    save data to plist file

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -9,8 +9,46 @@ import UIKit
 
 class ItemStore {
     var items : [[Item]] = [[], []]
+    let itemArchiveURL : URL = {
         
+        let documentDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentDirectories.first!
+        
+        return documentDirectory.appendingPathComponent("items.plist")
+    }()
+        
+    @objc func saveChanges() -> Bool {
+        do {
+            let encoder = PropertyListEncoder()
+
+            let data = try encoder.encode(items)
+            try data.write(to: itemArchiveURL, options: [.atomic])
+            print("Saved all items")
+            return true
+
+        } catch let encodingError { // explicit error name
+            print("Error coding item \(encodingError)")
+            return false
+        }
+    }
+    
+    init() {
+        // retrieve saved data
+        do {
+            let data = try Data(contentsOf: itemArchiveURL)
+            let unarchiver = PropertyListDecoder()
+            let items = try unarchiver.decode([[Item]].self, from: data)
+            self.items = items
+        } catch {
+            print("Error in retrieve data: \(error)")
+        }
+        // observe for scene sent to the background (app is closed) to save data
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(saveChanges), name: UIScene.didEnterBackgroundNotification, object: nil)
+    }
+    
     @discardableResult func createItem() -> Item {
+        // create random item and put it into 2 categories for 2 table sections: >$50 or other
         let item = Item(random: true)
         var section = 0
         switch item.valueDollars {
